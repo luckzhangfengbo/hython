c1a917f5c0a3e0b71c91d007d630334b57033d47
luckzhangfengbo <1819067326@qq.com>
2020-08-28 22:56:05 +0800

    2.hython-v1/ 2.hython-v1

---
+++ 2.hython-v1/include/AST.h
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: AST.h
+	> Author: huguang
+	> Mail: hug@haizeix.com
+	> Created Time: 四  8/27 16:28:23 2020
+ ************************************************************************/
+
+#ifndef _AST_H
+#define _AST_H
+
+#include <hythonLexer.h>
+#include <hythonParser.h>
+#include <string>
+
+namespace haizei {
+
+class ASTNode {
+public :
+    ASTNode(const char *file_name);
+    ASTNode(pANTLR3_BASE_TREE);
+    int size();
+    std::string text();
+    ASTNode operator[](int);
+    bool hasToken();
+    int type();
+    void destroy();
+    ~ASTNode();
+
+private:
+    pANTLR3_INPUT_STREAM input;
+    phythonLexer lex;
+    pANTLR3_COMMON_TOKEN_STREAM tokens;
+    phythonParser parser;
+    pANTLR3_BASE_TREE tree;
+    pANTLR3_COMMON_TOKEN tok;
+    
+    void init_tree(const char *file_name);
+};
+
+} // end of namespace haizei
+
+#endif
